497a3ebcf7c95de2eb75e608ae29063b16d32a33
Sourabh_Kumar_Sharma <sun.1945@rediffmail.com>
2014-12-04 08:58:55 +0530

    some commits

--- index.php
+++ index.php
@@ -23,6 +23,10 @@ $('.chatLiOpt').slideToggle('slow');
 
 return false ;/*We are using the return false here because if do not use then when the options div with the class class = "chatOpts" will work, but as it is contained inside the div with class = "chatTopH" it is getted minimized, to prevent that from happening we are using the return false*/
 });
+$('.proSpan').on('click', function(){
+	window.location = $(this).parent().attr('href');
+	return false;
+})
 })
 </script>
 </head>
@@ -30,7 +34,7 @@ return false ;/*We are using the return false here because if do not use then wh
 	<div class = "mainChatDiv" >
 		<div  class = 'chatTopH'  >
 			<div class  ="cmnDiv">
-			<a style = 'text-decoration:none;' href = 'http://www.google.com'><span style = 'color:white;font-size:18px;text-decoration:none;' >User Profile</span></a>
+			<a style = 'text-decoration:none;' href = 'http://www.google.com'><span class = 'proSpan' >User Profile</span></a>
 			</div>
 			<div>
 				<div class = 'chatOpts cmnDiv ' >
@@ -58,6 +62,36 @@ return false ;/*We are using the return false here because if do not use then wh
 			</div>
 		</div>
 	</div>
-	
+	<div class = "mainChatDiv" >
+		<div  class = 'chatTopH'  >
+			<div class  ="cmnDiv">
+			<a style = 'text-decoration:none;' href = 'http://www.google.com'><span class = 'proSpan' >User Profile</span></a>
+			</div>
+			<div>
+				<div class = 'chatOpts cmnDiv ' >
+				</div>
+				<div class = 'cmnDiv chatLiOpt'>
+					<div style = 'width:100%;background-color:lightblue;'>
+					Report Abusive
+					</div>
+					<div style = 'width:100%;background-color:lightgray;'>
+					Report Spam
+					</div>
+					<div style = 'width:100%;background-color:lightblue;'>
+					Delete
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		
+		<div class = 'chatTopD'>
+			<div class = 'cmnDiv chatMsgDiv'></div>
+			<div class = 'cmnDiv chatWriteDiv'>
+				<input style = 'width:100%;height:100%;' type = 'text' />
+			</div>
+		</div>
+	</div>
 </body>
 </html>
